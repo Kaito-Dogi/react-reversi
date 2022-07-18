78e7d98984deb4583ade8d1931ba3145777d6102
Kaito Dogi <kendobu0405@gmail.com>
2022-07-18 16:01:59 +0900

    :necktie: 盤面の初期化処理を実装

--- src/lib/generateInitialStatusesList.ts
+++ src/lib/generateInitialStatusesList.ts
@@ -1,4 +1,32 @@
 import { Status } from '../types/Status';
 
-export const generateInitialStatusesList = (n: number): Status[][] =>
-  Array<Status[]>(n).fill(Array<Status>(n).fill('NONE'));
+const DEFAULT_SIZE = 6;
+
+const generateStatusesList = (n: number): Status[][] =>
+  Array<Status[]>(n)
+    .fill(Array<Status>(n).fill('NONE'))
+    .fill(
+      Array<Status>(n)
+        .fill('NONE')
+        .fill('WHITE', n / 2 - 1, n / 2)
+        .fill('BLACK', n / 2, n / 2 + 1),
+      n / 2 - 1,
+      n / 2,
+    )
+    .fill(
+      Array<Status>(n)
+        .fill('NONE')
+        .fill('BLACK', n / 2 - 1, n / 2)
+        .fill('WHITE', n / 2, n / 2 + 1),
+      n / 2,
+      n / 2 + 1,
+    );
+
+export const generateInitialStatusesList = (n: number): Status[][] => {
+  // nは4以上の偶数でなければならない
+  if (n < 4 || n % 2 !== 0) {
+    return generateStatusesList(DEFAULT_SIZE);
+  }
+
+  return generateStatusesList(n);
+};
